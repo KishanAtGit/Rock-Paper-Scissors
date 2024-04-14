a72019e3ba50e4ff7ee4a53a3b271e21385d64ee
Uttam <kk82171@gmail.com>
2024-04-15 01:19:06 +0530

    first page frontend

---
+++ script.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("banner-openBtn")
+  .addEventListener("click", function () {
+    document.getElementById("banner").style.visibility = "visible";
+  });
+
+document
+  .getElementById("banner-closeBtn")
+  .addEventListener("click", function () {
+    document.getElementById("banner").style.visibility = "hidden";
+  });
