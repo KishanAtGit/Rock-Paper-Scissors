2555cf88204d067f70e58df264973065bea74f7b
Uttam <kk82171@gmail.com>
2024-04-17 01:41:02 +0530

    frontend,js  update

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ document
     document.getElementById("banner").style.visibility = "hidden";
   });
 
+const pageReload = () => location.reload();
+
 const computerChoice = () => {
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -18,33 +20,47 @@ const computerChoice = () => {
 };
 
 const result = (userChoice, pcChoice) => {
-  if (userChoice == pcChoice) return "tie";
+  console.log(userChoice, pcChoice);
+  if (userChoice == pcChoice) {
+    document.getElementById("message1").innerHTML = "TIE UP";
+    document.getElementById("message2").style.visibility = "hidden";
+    document.getElementById("play-again").innerHTML = "REPLAY";
+  } else if (
+    (userChoice == "rock" && pcChoice == "scissors") ||
+    (userChoice == "scissors" && pcChoice == "paper") ||
+    (userChoice == "paper" && pcChoice == "rock")
+  ) {
+    document.getElementById("next-button").style.display = "block";
+    document.getElementById("message1").innerHTML = "YOU WIN";
+  } else {
+    document.getElementById("message1").innerHTML = "YOU LOST";
+  }
 };
 
-const addUserChoice = choice => {
-  // console.log("working");
-  // console.log(choice);
+const addChoices = userChoice => {
   const rocks = document.querySelectorAll(".rock");
   const papers = document.querySelectorAll(".paper");
   const scissors = document.querySelectorAll(".scissors");
 
-  const userChoice = "";
-  const computer_choice = computerChoice();
+  let computer_choice = computerChoice();
   document.getElementById("the-game").style.display = "none";
   document.getElementById("result").style.display = "flex";
+  document.getElementById("result-message").style.display = "flex";
 
-  if (choice == "rock") rocks[0].style.display = "block";
-  else if (choice == "paper") papers[0].style.display = "block";
-  else if (choice == "scissors") scissors[0].style.display = "block";
+  if (userChoice == "rock") rocks[0].style.display = "block";
+  else if (userChoice == "paper") papers[0].style.display = "block";
+  else if (userChoice == "scissors") scissors[0].style.display = "block";
 
   if (computer_choice == 0) {
     rocks[1].style.display = "block";
-    userChoice = "rock";
+    computer_choice = "rock";
   } else if (computer_choice == 1) {
     papers[1].style.display = "block";
-    userChoice = "paper";
+    computer_choice = "paper";
   } else if (computer_choice == 2) {
     scissors[1].style.display = "block";
-    userChoice = "scissors";
+    computer_choice = "scissors";
   }
+
+  result(userChoice, computer_choice);
 };
